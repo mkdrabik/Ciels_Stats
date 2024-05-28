a96a548152fd48c941742cd89ed003936c6d78c2
mkdrabik <mkdrabik@unc.edu>
2024-05-28 13:37:31 -0400

    Fixes

--- src/App.js
+++ src/App.js
@@ -5,45 +5,53 @@ import v4 from "uuid"
 import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 
 function App() {
-  const[project, setProject] = useState({pm:"", product: "", summary:"", customer: "", kof: "", pb: "", link: "", ti: 0})
-  const pm = useRef("")
-  const prod = useRef("")
-  const sum = useRef("")
-  const cust = useRef("")
-  const kof = useRef("")
-  const pb = useRef("")
-  const ti = useRef(0)
-  const l = useRef("")
+  const[game, setGame] = useState({date: "", opponent: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, fouls: 0, win: ""})
+  const d = useRef("")
+  const o = useRef("")
+  const p = useRef(0)
+  const r = useRef(0)
+  const a = useRef(0)
+  const s = useRef(0)
+  const b = useRef(0)
+  const f = useRef(0)
+  const w = useRef("")
   var emp = true
 
   useEffect(()=>{
-    setProject({pm: "", product: "", summary: "", customer: "", kof: "", pb: "", link: "", ti: ""})
+    setGame({points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, fouls: 0, win: "", opponent:"", date: ''})
     clear()
   }, [])
   
 //Handle Data Upload
 const handleUpload = async () =>{
-  const valRef= collection(txtDB, 'IHM')
-  await setDoc(doc(txtDB, "IHM", project.product), 
-  {PM: project.pm, 
-    "Customer": project.customer,
-    "PM": project.pm,
-    "Key Objective Failures(Y/N)": project.kof,
-    "Procedure Bypasses": project.pb,
-    "Link": project.link,
-    "Timing Impact": project.ti
+  const valRef= collection(txtDB, 'AAU')
+  try{
+    game.date.toString()
+    await setDoc(doc(txtDB, "AAU", game.date.toString()), 
+  {
+    "Points": Number(game.points),
+    "Assists": Number(game.assists),
+    "Rebounds": Number(game.rebounds),
+    "Opponent": game.opponent,
+    "Steals": Number(game.steals),
+    "Blocks": Number(game.blocks),
+    "Win": game.win,
+    "Fouls": Number(game.fouls)
   })
   alert("Data added successfully.")
+} catch(err){
+  emp = true
+}
 }
 
 
   return (
     <>
-    <input placeholder='Enter PM Name'
+    <input placeholder='How many points'
     className = "new-item-form" 
-    type = 'text'
-    ref = {pm}
-    onChange={handlePMChange}
+    type = 'number'
+    ref = {p}
+    onChange={handlePointChange}
     />
     <button
     class='btn' 
@@ -55,10 +63,11 @@ const handleUpload = async () =>{
     </button>
 
     <br></br><br></br>
-    <input placeholder='Enter Product Name'
-    className = "new-item-form" 
-    ref = {prod}
-    onChange={handleProductChange}
+    <input placeholder='How many rebounds'
+    className = "new-item-form"
+    type = 'number' 
+    ref = {r}
+    onChange={handleReboundChange}
     />
     <button
     class='btn-clear' 
@@ -72,100 +81,113 @@ const handleUpload = async () =>{
     <br></br><br></br>
     <input
     className = "new-item-form" 
-    placeholder='Enter Customer'
-    ref = {cust}
-    onChange={handleCustomerChange}/>
+    placeholder= 'How many assists'
+    type = 'number'
+    ref = {a}
+    onChange={handleAssistChange}/>
     <br></br><br></br>
 
-    <input placeholder='Enter Summary'
-    ref = {sum}
-    onChange={handleSummaryChange}/>
+    <input placeholder= 'How many steals'
+    ref = {s}
+    type = 'number'
+    onChange={handleStealChange}/>
     <br></br><br></br>
 
-    <input placeholder='Were there Key Objective Failures(Y/N)'
-    ref = {kof}
-    onChange={handleKOFChange}/>
+    <input placeholder='How many blocks'
+    ref = {b}
+    type = 'number'
+    onChange={handleBlockChange}/>
     <br></br><br></br>
 
-    <input placeholder='Were there Procedure Bypasses(Y/N)'
-    ref = {pb}
-    onChange={handlePBChange}/>
+    <input placeholder='How many fouls'
+    ref = {f}
+    type = 'number'
+    onChange={handleFoulChange}/>
     <br></br><br></br>
 
-    <input placeholder='Paste Link'
-    ref = {l}
-    onChange={handleLinkChange}/>
+    <input placeholder='Win?'
+    ref = {w}
+    type = 'text'
+    onChange={handleWinChange}/>
+    <br></br><br></br>
+
+    <input placeholder='Opponent?'
+    ref = {o}
+    type = 'text'
+    onChange={handleOpponentChange}/>
     <br></br><br></br>
 
     <input placeholder='Enter Timing Impact'
-    type = 'number'
-    ref = {ti}
-    onChange={handleTimeImpactChange}/>
+    type = 'date'
+    ref = {d}
+    onChange={handleDateChange}/>
     <br></br><br></br>
     </>
   );
   
 
 
+//Handle Data entry functions
+  function handlePointChange(e){
+    setGame(g => ({...game, points: e.target.value}))
+  }
+  function handleReboundChange(e){
+    setGame(g => ({...game, rebounds: e.target.value}))
+  }
+  function handleStealChange(e){
+    setGame(g => ({...game, steals: e.target.value}))
+  }
+  function handleAssistChange(e){
+    setGame(g=> ({...game, assists: e.target.value}))
+  }
+  function handleBlockChange(e){
+    setGame(g=> ({...game, blocks: e.target.value}))
+  }
+  function handleFoulChange(e){
+    setGame(g=> ({...game, fouls: e.target.value}))  
+  }
+  function handleWinChange(e){
+    setGame(g=> ({...game, win: e.target.value}))  
+  }
+  function handleDateChange(e){
+    setGame(g=> ({...game, date: e.target.value}))  
+  }
+  function handleOpponentChange(e){
+    setGame(g=> ({...game, opponent: e.target.value}))  
+  }
+  
 
-
-
-{//Handle Data entry functions
-}
-  function handlePMChange(e){
-    setProject(p => ({...project, pm: e.target.value}))
-  }
-  function handleProductChange(e){
-    setProject(p => ({...project, product: e.target.value}))
-  }
-  function handleSummaryChange(e){
-    setProject(p => ({...project, summary: e.target.value}))
-  }
-  function handleCustomerChange(e){
-    setProject(p=> ({...project, customer: e.target.value}))
-  }
-  function handleKOFChange(e){
-    setProject(p=> ({...project, kof: e.target.value}))
-  }
-  function handlePBChange(e){
-    setProject(p=> ({...project, pb: e.target.value}))  
-  }
-  function handleLinkChange(e){
-    setProject(p=> ({...project, link: e.target.value}))  
-  }
-  function handleTimeImpactChange(e){
-    setProject(p=> ({...project, ti: e.target.value}))  
-  }
-
-{//Checks to see if all fields are filled then calls handle upload
-}
+//Checks to see if all fields are filled then calls handle upload
   function filled(){
     emp = false
-    for (let key in project){
-      if(project[key] === ''){
+    for (let key in game){
+      if(game[key] === '' || game[key]=== null){
         emp = true
         break
       }
     }
     if(emp === false ){
       handleUpload()
-      console.log({project}); setProject({pm: "", product: "", summary: "", customer: "", link: ""})
+      if(emp===false){
       clear()
+      } else {
+        alert("Enter a valid date")
+      }
     }
   }
 
-{//Sets everything empty
-}
+//Sets everything empty
   function clear(){
-    pm.current.value = ''
-    prod.current.value = ''
-    sum.current.value = ''
-    cust.current.value = ''
-    kof.current.value = ''
-    pb.current.value = ''
-    l.current.value = ''
-    ti.current.value = ''
-    setProject({pm: "", product: "", summary: "", customer: "", kof: "", pb: "", link: "", ti: ''})
+    p.current.value = ''
+    r.current.value = ''
+    a.current.value = ''
+    f.current.value = ''
+    s.current.value = ''
+    b.current.value = ''
+    d.current.value = ''
+    w.current.value = ''
+    o.current.value = ''
+    setGame({points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, fouls: 0, win: '', opponent: ''})
   }
 
 
