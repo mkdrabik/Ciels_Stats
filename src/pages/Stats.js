5eb296a019e3c2b4d2bd94b41d551f5984ca834a
mkdrabik <mkdrabik@unc.edu>
2024-06-05 14:03:22 -0400

    early stats

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,11 +1,30 @@
 import "./Stat_Form.css";
 import Header from "../components/Header";
+import { txtDB } from "./txtConfig";
+import { collection, getDocs } from "firebase/firestore";
+
+const colRef = collection(txtDB, "IHM");
+const read = () => {
+  getDocs(colRef).then((snapshot) => {
+    let games = [];
+    snapshot.docs.forEach((doc) => {
+      games.push({ id: doc.id });
+    });
+    console.log(games);
+  });
+};
 
 function Stats() {
   return (
     <div>
       <Header />
-      <h1>Stats</h1>
+      <button
+        onClick={(e) => {
+          read();
+        }}
+      >
+        Stats
+      </button>
     </div>
   );
 }
