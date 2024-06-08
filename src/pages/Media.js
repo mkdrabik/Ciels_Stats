22bf8ed9fe390d88d2fe6fa447e88cdb9aaf234a
Mason Drabik <mkdrabik@unc.edu>
2024-06-07 23:46:28 -0400

    some pics

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -6,6 +6,19 @@ function Media() {
     <div>
       <Header />
       <h1>Pictures/Video</h1>
+      <iframe
+        src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fsarahzak32%2Fvideos%2F1661735077686269%2F&show_text=false&width=267&t=0"
+        width="267"
+        height="476"
+        style={{ float: "border:none;overflow:hidden" }}
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        allowFullScreen="true"
+        title="shot"
+      ></iframe>
+      <img src={require("./c1.jpeg")} alt="Logo" />;
     </div>
   );
 }
