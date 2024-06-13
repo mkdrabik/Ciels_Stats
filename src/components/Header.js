4d87377964377ce94dda2acc813f769f29de6213
mkdrabik <mkdrabik@unc.edu>
2024-06-13 16:37:30 -0400

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     <div class="header">
       <h1>
         <Link to="/about" className="cname">
-          Mason Drabik
+          Cecilia Drabik
         </Link>
       </h1>
       <nav class="navbar">
