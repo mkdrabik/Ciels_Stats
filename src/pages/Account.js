c4ac933354c6babc0d055e3ea5c1789668a72ffb
mkdrabik <mkdrabik@unc.edu>
2024-06-13 16:49:53 -0400

    I am him. (I have no idea how I got this to work)

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,6 +1,7 @@
 import { React } from "react";
 import Header from "../components/Header";
-import { auth } from "../pages/txtConfig";
+import { txtDB, auth } from "../pages/txtConfig";
+import { doc, setDoc } from "firebase/firestore";
 import "./css/Account.css";
 import {
   signOut,
