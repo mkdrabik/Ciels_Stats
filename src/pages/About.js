0217a613aad8e656124bb337768fc828fd73cfb7
mkdrabik <mkdrabik@unc.edu>
2024-06-13 13:22:28 -0400

    css no bueno

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,33 +3,33 @@ import Header from "../components/Header";
 
 function About() {
   return (
-    <body className="bg2">
+    <body>
       <Header />
-      <h1 className="ab1">About: </h1>
+
       <div className="hori">
         <h1 className="ab1">Height:</h1>
-        <h1 className="ab"> 5'6"</h1>
+        <h1 className="ab2"> 5'6"</h1>
       </div>
 
       <div className="hori">
         <h1 className="ab1">Position: </h1>
-        <h1 className="ab"> PG/SG</h1>
+        <h1 className="ab2"> PG/SG</h1>
       </div>
       <div className="hori">
         <h1 className="ab1">AAU Team: </h1>
-        <h1 className="ab"> NC Steelers</h1>
+        <h1 className="ab2"> NC Steelers</h1>
       </div>
       <div className="hori">
         <h1 className="ab1">School: </h1>
-        <h1 className="ab"> Immaculate Heart of Mary</h1>
+        <h1 className="ab2"> Immaculate Heart of Mary</h1>
       </div>
       <div className="hori">
         <h1 className="ab1">GPA: </h1>
-        <h1 className="ab"> 4.0</h1>
+        <h1 className="ab2"> 4.0</h1>
       </div>
       <div className="hori">
         <h1 className="ab1"> Playstyle: </h1>
-        <h1 className="ab">
+        <h1 className="ab2">
           {" "}
           Team first player who is constantly trying to get teamates involved.
           Scoring comes from slashing and 3pt shooting. Teacious defender who is
--- src/pages/Media.js
+++ src/pages/Media.js
@@ -6,8 +6,8 @@ function Media() {
   return (
     <body className="abg">
       <Header />
-      <div>
-        <h1 className="ab">Pictures/Video</h1>
+      <h1 className="ab">Pictures/Video</h1>
+      <div className="img-track">
         <iframe
           src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fsarahzak32%2Fvideos%2F1661735077686269%2F&show_text=false&width=267&t=0"
           width="267"
@@ -20,7 +20,7 @@ function Media() {
           allowFullScreen="true"
           title="shot"
         ></iframe>
-        <img src={require("./imgs/c1.jpeg")} alt="Logo" />;
+        <img src={require("./imgs/c1.jpeg")} alt="Logo" draggable="false" />;
       </div>
     </body>
   );
