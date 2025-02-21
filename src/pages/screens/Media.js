cfb666c3b8e9ca8dbaa9591772647b6925d21158
Mason Drabik <mkdrabik@unc.edu>
2025-02-20 22:50:20 -0500

    gw add

--- src/pages/screens/Media.js
+++ src/pages/screens/Media.js
@@ -9,6 +9,18 @@ function Media() {
       <h1 className="ab">IHM</h1>
       <div className="img-track">
         <iframe
+          src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fsarahzak32%2Fvideos%2F1678779583034580%2F&show_text=false&width=267&t=0"
+          width="267"
+          height="476"
+          scrolling="no"
+          style={{ float: "border:none;overflow:hidden" }}
+          frameborder="0"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          allowFullScreen="true"
+          title="gw"
+        ></iframe>
+        <iframe
           src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fsarahzak32%2Fvideos%2F1661735077686269%2F&show_text=false&width=267&t=0"
           width="267"
           height="476"
